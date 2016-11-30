6d87ca0edf6a38cb980c33622ec3a3b998144c5c
Luis <siquike@gmail.com>
2016-11-29 19:14:36 -0500

    Starting homework

---
+++ code/InitializeNetwork.m
@@ -0,0 +1,10 @@
+function [W, b] = InitializeNetwork(layers)
+% InitializeNetwork([INPUT, HIDDEN1, HIDDEN2, ..., OUTPUT]) initializes the weights and biases
+% for a fully connected neural network with input data size INPUT, output data
+% size OUTPUT, and in between are the number of hidden units in each of the layers.
+% It should return the cell arrays 'W' and 'b' which contain the randomly
+% initialized weights and biases for this neural network.
+
+
+
+end
